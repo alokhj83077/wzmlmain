f1e67665db426144ddc84036b0c04728bfe400db
alokhj83077 <113902349+alokhj83077@users.noreply.github.com>
2022-09-25 18:07:41 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -238,7 +238,6 @@ def get_readable_message():
                 else:
                     msg += f"\n<b>Engine :</b> {download.eng()}"
                     msg += f"\n<b>Size: </b>{download.size()}"
-            msg += f"\n<b>_____________________________________</b>"
             msg += "\n\n"
             if STATUS_LIMIT is not None and index == STATUS_LIMIT:
                 break
