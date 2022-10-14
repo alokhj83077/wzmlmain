f468c0ca9f5d4d02bff3f2dbb4cdb15ee51f1a4c
alokhj83077 <113902349+alokhj83077@users.noreply.github.com>
2022-10-14 20:42:46 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -385,7 +385,7 @@ class GoogleDriveHelper:
                 else:
                     msg += f'<b>Name: </b><code>{meta.get("name")}</code>'
                     msg += f'\n\n<b>Size: </b>{get_readable_file_size(self.transferred_size)}'
-                    msg += '\n\n<b>Type: </b>Folder'
+                    msg += f'\n\n<b>Type: </b>Folder'
                     msg += f'\n<b>SubFolders: </b>{self.__total_folders}'
                     msg += f'\n<b>Files: </b>{self.__total_files}'
                 buttons = ButtonMaker()
